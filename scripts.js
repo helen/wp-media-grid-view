90d4024d606e368479ca9cb14fd5bb0e060efc21
Shaun Andrews <shaun@automattic.com>
2013-09-12 16:13:21 -0400

    Adding some better looking styles for media items. Added a slider to dynamically change the size of the thumbnails. Mocked up the delete interaction.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,19 @@
 var wpMediaGrid;
+var timeoutId;
 
 (function($) {
 	wpMediaGrid = {
 		init: function() {
+			// Size Slider
+			wpMediaGrid.changeThumbSize( 1 );
+			$(".thumbnail-size input").bind("slider:changed", function (event, data) {
+				wpMediaGrid.changeThumbSize( data.value );
+			});
+
+			// Toggle Select Items
 			$( '.media-item' ).on('click', function() {
 				var id = $(this).data('id'),
-					details = $(this).find( '.media-details' ).html(),
+					details = $(this).find( '.media-details' ),
 					selected = $('#selected-media-details .selected-media');
 
 				if ( $(this).hasClass('selected') ) {
@@ -13,18 +21,78 @@ var wpMediaGrid;
 					selected.find( '#detail-' + id ).remove();
 				} else {
 					$(this).addClass('selected');
-					selected.prepend('<li class="selected-details" id="detail-' + id + '" data-id="' + id + '">' + details + '</li>');
+					selected.prepend('<li class="selected-details" id="detail-' + id + '" data-id="' + id + '">' + details.html() + '</li>');
+					selected.find('#detail-' + id + ' .media-options').hide();
+					selected.find('#detail-' + id + ' h3').show();
 				}
 
 				wpMediaGrid.selectedCount();
 			});
 
+			// Click to unselect from sidebar
 			$( '#selected-media-details' ).on( 'click', '.selected-details', function() {
 				var id = $(this).data('id'),
 					item = $('#media-' + id);
 				$(this).remove();
 				item.removeClass('selected');
+				wpMediaGrid.selectedCount();
 			});
+
+			// Delayed Actions for Mouseover on Items
+			$('.media-item').on('mouseover', function() {
+				var item = $(this);
+				if (!timeoutId) {
+					timeoutId = window.setTimeout(function() {
+						item.find('.media-details .media-options').fadeIn('fast');
+					}, 750);
+				}
+			});
+			$('.media-item').on('mouseleave', function() {
+				$(this).find('.media-details .media-options').hide();
+				if (timeoutId) {
+					window.clearTimeout(timeoutId);
+					timeoutId = null;
+				}
+			});
+
+			// Delete Item
+			$( '.media-item' ).on( 'click', '.media-delete', function( event ) {
+				event.preventDefault();
+				event.stopPropagation();
+				var item = $(this).closest( '.media-item' );
+				item.css({
+					'opacity': '0',
+					'margin-left': '-200px'
+				});
+				setTimeout( function() {
+					if( item.hasClass( 'selected' ) ) {
+						item.trigger( 'click' );
+					}
+					item.remove();
+				}, 300);
+			});
+		},
+
+		changeThumbSize: function(ratio) {
+			var container_size = 200 * ratio,
+				thumb_size = 180 * ratio,
+				containers = $( '.media-item' ),
+				thumbs = containers.find( '.media-thumb' )
+				images = thumbs.find( 'img' );
+
+			containers.height( container_size );
+			containers.width( container_size );
+
+			thumbs.height( thumb_size );
+			thumbs.width( thumb_size );
+
+			images.each( function(index) {
+				$( this ).removeClass('default');
+				og_height = $(this).data('height');
+				og_width = $(this).data('width');
+				$( this ).height( og_height * ratio );
+				$( this ).width( og_width * ratio );
+			} );
 		},
 
 		selectedCount: function() {
--- wp-media-grid.php
+++ wp-media-grid.php
@@ -12,7 +12,8 @@ class WP_Media_Grid {
 	function __construct() {
 
 		add_action( 'load-upload.php',  array( $this, 'media_grid' ) );
-		add_action( 'admin_print_scripts-upload.php', array( $this, 'enqueue' ) );
+		//add_action( 'admin_print_scripts-upload.php', array( $this, 'enqueue' ) );
+		add_action( 'admin_init', array( $this, 'enqueue' ) );
 	}
 
 	/**
@@ -30,7 +31,8 @@ class WP_Media_Grid {
 			'post_type' => 'attachment',
 			'post_mime_type' =>'image',
 			'post_status' => 'inherit',
-			'posts_per_page' => 150,
+			'posts_per_page' => 75,
+			'paged' => 1,
 		);
 
 		$images = new WP_Query( $args );
@@ -42,23 +44,29 @@ class WP_Media_Grid {
 				<?php /* <pre><?php var_dump($image); ?></pre> */ ?>
 				<li class="media-item" id="media-<?php echo $image->ID; ?>" data-id="<?php echo $image->ID; ?>">
 					<div class="media-thumb">
-						<?php echo wp_get_attachment_image( $image->ID, array(200,200) ); ?>
+						<?php $img_attr = wp_get_attachment_image_src( $image->ID, array(180,180) ); ?>
+						<img class="default" src="<?php echo $img_attr[0]; ?>" width="$img_attr[1]" height="$img_attr[2]" data-width="<?php echo $img_attr[1]; ?>" data-height="<?php echo $img_attr[2]; ?>">
 					</div>
 					<div class="media-details">
 						<?php echo wp_get_attachment_image( $image->ID, array(35,35) ); ?>
 						<h3><?php echo $image->post_title; ?></h3>
 						<ul class="media-options">
-							<li><a href="#">Edit</a></li>
-							<li><a href="#">Delete</a></li>
+							<li><a class="media-edit" href="#">Edit</a></li>
+							<li><a class="media-delete" href="#">Delete</a></li>
 						</ul>
 					</div>
 				</li>
 			<?php endforeach; ?>
 			</ol>
 			<div id="selected-media-details">
+				<fieldset class="thumbnail-size">
+					<input type="text" data-slider="true" data-slider-step="0.1" data-slider-snap="false" value="1" data-slider-range="0.7,2">
+				</fieldset>
 				<h2>Selected Media</h2>
 				<ul class="selected-media-options">
 					<li class="selected-count"><strong>0</strong> items selected</li>
+					<li><a href="#">Compare</a></li>
+					<li><a href="#">Tag</a></li>
 					<li><a href="#">Download</a></li>
 					<li><a href="#">Delete</a></li>
 				</ul>
@@ -77,9 +85,11 @@ class WP_Media_Grid {
 	 * Enqueue scripts and styles
 	 */
 	public function enqueue() {
-		// Relies on Backbone.js
-		wp_enqueue_script( 'wp-media-grid', plugins_url( 'scripts.js', __FILE__ ) );
+		wp_enqueue_script( 'wp-media-grid', plugins_url( 'scripts.js', __FILE__ ), array( 'jquery' ) );
 		wp_enqueue_style( 'wp-media-grid', plugins_url( 'styles.css', __FILE__ ) );
+
+		wp_enqueue_script( 'wp-size-slider', plugins_url( 'libs/simple-slider.min.js', __FILE__ ) );
+		wp_enqueue_style( 'wp-size-slider', plugins_url( 'libs/simple-slider.css', __FILE__ ) );
 	}
 }
 
